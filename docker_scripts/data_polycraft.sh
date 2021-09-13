fb54693a7058814ee3baff27ba00e416adf41560
PatrickFeeney <patrickfeeney1225@gmail.com>
2021-09-13 14:39:40 -0400

    Polycraft Scripts for Both Servers

---
+++ docker_scripts/data_polycraft.sh
@@ -0,0 +1,8 @@
+# set Pipenv to use in this script
+export PIPENV_PIPFILE=${CODE_DIR}/Pipfile
+# run visual novelty detector
+cd ${CODE_DIR}/polycraft_tufts/Novelty_Detectors/polycraft_detector/
+pipenv run python3 data_server.py &
+# run Polycraft, with config.py set to launch ADE agent
+cd ${CODE_DIR}/PAL/PolycraftAIGym
+pipenv run python3 LaunchTournament.py
